14a954a40579c8fe5e2e5cc0356988a71908ecc5
Marlon Israel <marlonisrael11@hotmail.com>
2019-10-12 08:18:49 -0500

    corri

--- proyectoBase/src/app/app-routing.module.ts
+++ proyectoBase/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { RegistrarPage } from './auth/registrar/registrar.page';
-<<<<<<< HEAD
 
 const routes: Routes = [
   {
@@ -11,18 +10,6 @@ const routes: Routes = [
   { path: 'registro', component: RegistrarPage },
   { path: 'registrar', component: RegistrarPage  },
   { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' }
-=======
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'registrar', component: RegistrarPage },
-  { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' }
-
->>>>>>> ba67054aa114e8ad4e647379d009e1023e4be870
 ];
 @NgModule({
   imports: [
@@ -30,8 +17,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-<<<<<<< HEAD
 export class AppRoutingModule {}
-=======
-export class AppRoutingModule {}
->>>>>>> ba67054aa114e8ad4e647379d009e1023e4be870
--- proyectoBase/src/app/app.module.ts
+++ proyectoBase/src/app/app.module.ts
@@ -8,17 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
->>>>>>> ba67054aa114e8ad4e647379d009e1023e4be870
 
 import { HttpClientModule } from '@angular/common/http';
 import { RegistrarPage } from './auth/registrar/registrar.page';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [ AppComponent, RegistrarPage],
   entryComponents: [],
   imports: [BrowserModule,
@@ -28,16 +23,6 @@ import { RegistrarPage } from './auth/registrar/registrar.page';
             ReactiveFormsModule,
             HttpClientModule
           ],
-=======
-  declarations: [AppComponent,RegistrarPage],
-  entryComponents: [],
-  imports: [BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule],
->>>>>>> ba67054aa114e8ad4e647379d009e1023e4be870
   providers: [
     StatusBar,
     SplashScreen,
