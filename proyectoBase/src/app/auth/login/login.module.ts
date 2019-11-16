b4f4254ddd71c3f0ea46b706e996e3ef098eabe9
Marlon Israel <marlonisrael11@hotmail.com>
2019-11-15 22:46:14 -0600

    Cuenta y mas

--- proyectoBase/src/app/auth/login/login.module.ts
+++ proyectoBase/src/app/auth/login/login.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+
 const routes: Routes = [
   {
     path: '',
